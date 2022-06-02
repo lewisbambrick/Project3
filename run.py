aa5350c041603603e917aa761c6c28014c74275a
lewisbambrick <lewis.bambrick@gmail.com>
2022-06-02 20:00:28 +0000

    gatherstock data and add to spreadsheet

--- run.py
+++ run.py
@@ -71,13 +71,29 @@ def calculate_surplus_data(sales_row):
 
 def get_last_5_entries_sales():
     sales = SHEET.worksheet("sales")
+
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
-        column.append(column[-5:])
+        columns.append(column[-5:])
 
     return columns
-    
+
+
+def calculate_stock_data(data):
+
+    print("Calculating Stock Data...\n")
+
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
 
 def main():
 
@@ -88,6 +104,9 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
     update_spreadsheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_spreadsheet(stock_data, "stock")
 
 
 main()
